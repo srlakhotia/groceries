40d4f25be7e5e0ce443ea2f5fe7903d61a059dc2
Sneh Raj Lakhotia <slakhotia@sapient.com>
2018-12-03 13:16:18 +0530

    inital setup commit

---
+++ src/reducers/index.js
@@ -0,0 +1,15 @@
+const task = (state, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+};
+
+const tasks = (state = [], action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+};
+
+export default tasks;
