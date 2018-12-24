a1e5e5e5beece4885bd4d61ad7e2894f83973517
Sneh Raj Lakhotia <slakhotia@sapient.com>
2018-12-24 16:04:02 +0530

    Home page main content start

---
+++ src/containers/home.container.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import propTypes from 'prop-types';
+
+
+function Home (props) {
+    return (
+        <React.Fragment>
+            jjj
+        </React.Fragment>
+    )
+}
+
+export default Home;
--- src/containers/mainContent.container.js
+++ src/containers/mainContent.container.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import propTypes from 'prop-types';
+import  {BrowserRouter as Router, Route} from 'react-router-dom';
+import Home from './home.container';
 
 
 function MainContent (props) {
     return (
-        <div>Asaa</div>
+        <Router>
+            <Route exact path="/" render={() => <Home></Home>} />
+        </Router>
     )
 }
 
